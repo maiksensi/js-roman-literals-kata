eb9ce3743896bc400d9b3ba5b2759f0165268177
Lilian Held <Lilian.Held-extern@deutschebahn.com>
2020-05-22 14:26:37 +0200

    wip

--- src/literals.spec.js
+++ src/literals.spec.js
@@ -4,4 +4,8 @@ describe("roman literals", () => {
   it("should convert 1 to I", () => {
     expect(numberToRoman(1)).toEqual("I")
   })
+
+  it('should convert 2 to II', () => {
+    expect(numberToRoman(2)).toEqual("II")
+  })
 })
