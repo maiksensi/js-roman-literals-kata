5f7aef22e1cfd598a2ec55aa2b990a8686a2b590
Tommy Bluhm <tommy.bluhm@gmail.com>
2020-05-22 14:36:42 +0200

    wip

--- src/literals.spec.js
+++ src/literals.spec.js
@@ -8,4 +8,13 @@ describe("roman literals", () => {
   it('should convert 2 to II', () => {
     expect(numberToRoman(2)).toEqual("II")
   })
+
+  it('should convert 4 to IV', () => {
+    expect(numberToRoman(4)).toEqual("IV")
+  })
 })
+
+M = 1000
+V = 5
+IV = 4
+III = 3
